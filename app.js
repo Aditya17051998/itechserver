1b52daad2c7c06dca041469e98103b02840a761e
Aditya Kushwaha <kushwahaa862@gmail.com>
2021-08-27 14:18:26 +0530

    fixing issue

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 
-const port=process.env || process.env.APP_PORT
+const port=process.env.PORT || process.env.APP_PORT
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const swaggerUI = require("swagger-ui-express");
+// const swaggerUI = require("swagger-ui-express");
 const passport = require("passport");
 const session = require("express-session");
 
-const swagger = require("./lib/swagger");
+// const swagger = require("./lib/swagger");
 const mainRoute = require("./routes/index");
-const authRoute = require("./routes/authentication/index");
-const adminRoute = require("./routes/admin/index");
-const courseRoute = require("./routes/course/index");
-const sectionRoute = require("./routes/course/section");
-const lectureRoute = require("./routes/course/lecture");
-const assignmentRoute = require("./routes/assignment/index");
-const courseStudentRoute = require("./routes/course/student");
+// const authRoute = require("./routes/authentication/index");
+// const adminRoute = require("./routes/admin/index");
+// const courseRoute = require("./routes/course/index");
+// const sectionRoute = require("./routes/course/section");
+// const lectureRoute = require("./routes/course/lecture");
+// const assignmentRoute = require("./routes/assignment/index");
+// const courseStudentRoute = require("./routes/course/student");
 
 
 
@@ -40,22 +40,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //     })
 // );
 app.use(cors());
-app.use(
-    session({
-        name: "sid",
-        secret: "qwertyuiopasdfghjklzxcvbnm",
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            SameSite: "None",
-            Secure: true,
-            maxAge: 60000000
-        },
-    })
-);
-app.use(cookieParser("qwertyuiopasdfghjklzxcvbnm"));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(
+//     session({
+//         name: "sid",
+//         secret: "qwertyuiopasdfghjklzxcvbnm",
+//         resave: false,
+//         saveUninitialized: true,
+//         cookie: {
+//             SameSite: "None",
+//             Secure: true,
+//             maxAge: 60000000
+//         },
+//     })
+// );
+// app.use(cookieParser("qwertyuiopasdfghjklzxcvbnm"));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+////not usable//////////////
 // app.use(function (req, res, next) {
 //     req.sessionOptions.maxAge = 24 * 60 * 60 * 1000;
 //     next()
@@ -63,18 +65,18 @@ app.use(passport.session());
 
 
 app.use("/", mainRoute);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
-app.use("/api/authentication", authRoute);
-app.use("/api/admin", adminRoute);
-app.use("/api/course", courseRoute);
-app.use("/api/course", sectionRoute);
-app.use("/api/course", lectureRoute);
-app.use("/api/course", assignmentRoute);
-app.use("/api/course", courseStudentRoute);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swagger));
+// app.use("/api/authentication", authRoute);
+// app.use("/api/admin", adminRoute);
+// app.use("/api/course", courseRoute);
+// app.use("/api/course", sectionRoute);
+// app.use("/api/course", lectureRoute);
+// app.use("/api/course", assignmentRoute);
+// app.use("/api/course", courseStudentRoute);
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`app is running at ${process.env.APP_PORT}`);
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
 });
 
 
--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ config.sessionTimeoutTime = 1800;
 config.databaseMicroService = {
   type: "tcp",
   port: process.env.DB_MICROSERVICE_PORT,
-  host: "microservice12.herokuapp",
-  protocol: "https",
+  host: "localhost",
+  protocol: "http",
   timeout: 180000,
 };
 
